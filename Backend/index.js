09ad79ae2d9fda9f0a771022ab89215ea921a1c0
naman katiyar <naman.20233196@mnnit.ac.in>
2024-11-13 15:06:19 +0530

    frontendwithsome backend

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+import express from"express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bookRoute from "./route/book.route.js";
+const app = express()
+dotenv.config();
+const PORT=process.env.PORT ||4000;
+const URI=process.env.MongoDBURI;
+//connect to mongo db
+try{
+  mongoose.connect(URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  });
+  console.log("connected to mongo db")
+
+}
+catch(error){
+  console.log("Error:",error);
+}
+//defining rouute
+app.use("/book",bookRoute)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
